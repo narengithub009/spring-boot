1418852dfe80822239e15953ecdc99d8382ede32
narengithub009 <narender.kon09@gmail.com>
2023-12-16 10:07:34 +0530

    first commit

---
+++ src/main/java/com/java/springboot/Application.java
@@ -0,0 +1,24 @@
+package com.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/test")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/msg")
+	public String getMessage() {
+		System.out.println("Hello World.......");
+		return "Hello World....";
+	}
+
+}
