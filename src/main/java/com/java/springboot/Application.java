9eea8d7ac4844be70b8b685a311a08ab1d57262c
narengithub009 <narender.kon09@gmail.com>
2023-12-16 18:36:22 +0530

    Method url modification

--- src/main/java/com/java/springboot/Application.java
+++ src/main/java/com/java/springboot/Application.java
@@ -20,7 +20,7 @@ public class Application {
 		System.out.println("Hello World.......");
 		return "Hello World....";
 	}
-	@GetMapping("/msgBranch")
+	@GetMapping("/msgBranchModified")
 	public String getBranchMessage() {
 		System.out.println("Hello getBranchMessage World.......");
 		return "Hello getBranchMessage World....";
