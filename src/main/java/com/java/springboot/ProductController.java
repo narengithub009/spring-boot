66805085b61b69d607903f521a80cb9289e48927
narengithub009 <narender.kon09@gmail.com>
2023-12-23 22:12:50 +0530

    Added response Entity

--- src/main/java/com/java/springboot/ProductController.java
+++ src/main/java/com/java/springboot/ProductController.java
@@ -3,6 +3,9 @@ package com.java.springboot;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/product")
 public class ProductController {
 	@GetMapping("/products")
-	public List<String> getProducts() {
+	public ResponseEntity<List<String>> getProducts() {
 		
-		return  Arrays.asList("Phone","Pen","TV");
+		return  new ResponseEntity(Arrays.asList("Phone","Pen","TV","Books"),HttpStatus.CREATED);
 	}
 }
