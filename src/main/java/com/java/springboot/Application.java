d4c1dc31732e7366ed822ee6ccdbb74976d27105
narengithub009 <narender.kon09@gmail.com>
2023-12-26 17:24:43 +0530

    Added method response

--- src/main/java/com/java/springboot/Application.java
+++ src/main/java/com/java/springboot/Application.java
@@ -2,6 +2,8 @@ package com.java.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +24,10 @@ public class Application {
 	}
 
 	@GetMapping("/msgBranch")
-	public String getBranchMessage() {
+	public ResponseEntity<String> getBranchMessage() {
 		
 		System.out.println("Hello  World.......");
 		
-		return "Hello getBranchMessage World....";
+		return new ResponseEntity("Hello getBranchMessage World....",HttpStatus.OK);
 	}
 }
